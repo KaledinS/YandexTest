f29bef9383a23c6fbc9c97fd512880962c8884af
kaledinS <36567900+srnat@users.noreply.github.com>
2019-04-30 18:10:15 +0300

    создан новый проект

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -3,23 +3,28 @@ import org.junit.*;
 public class RunTest {
 
     @Before
-    public void beforeTest(){}
-
-    @Test
-    public void test(){
-        String string1 = "cтрока";
-        String string2 = "cтрока";
-        Assert.assertEquals(string1,string2);
+    public void beforeTest() {
+        System.out.println("before");
     }
 
     @Test
-    public void badTest(){
+    public void test() {
+        System.out.println("test");
+        String string1 = "cтрока";
+        String string2 = "cтрока";
+        Assert.assertEquals(string1, string2);
+    }
+
+    @Test
+    public void badTest() {
+        System.out.println("badTest");
         String string1 = "cтрока";
         String string2 = "cтрока1";
-        Assert.assertEquals(string1,string2);
+        Assert.assertEquals(string1, string2);
     }
 
     @After
-    public void afterTest(){}
-
+    public void afterTest() {
+        System.out.println("after");
+    }
 }
